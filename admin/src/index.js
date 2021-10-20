e805c0ab0ac2b1e59602977595902166bfd07ce2
Stephen Tippets <stephen.tippets@gmail.com>
2021-10-19 22:11:28 -0600

    Add sentry tracking to admin and client

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import {Integrations} from '@sentry/tracing'
 import './styles/global.css'
 import App from './app'
 import AppProviders from './context'
 import reportWebVitals from './reportWebVitals'
+
+
+Sentry.init({
+  dsn: 'https://399a3366e6dc49d9bdb80014e727aff5@o1025860.ingest.sentry.io/6020369',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react'
+import {Integrations} from '@sentry/tracing'
 import {App} from './app';
 import { AppProviders } from './context';
+
+Sentry.init({
+  dsn: 'https://ed049864dbe9466989408ae29f6ceeab@o1025860.ingest.sentry.io/6019941',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <AppProviders>
@@ -9,8 +21,3 @@ ReactDOM.render(
   </AppProviders>,
   document.getElementById("root")
 );
-
-// ReactDOM.render(
-//     <App />,
-//   document.getElementById("root")
-// );
